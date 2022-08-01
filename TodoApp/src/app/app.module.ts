09bd41e6ec0132a39795b55851d1ac5b2ed07b22
deekeej <daniel.krejza@gmail.com>
2022-08-01 17:35:41 +0200

    generate project and divide to basic parts

---
+++ TodoApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoItemsComponent } from './components/todo-items/todo-items.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoInputComponent,
+    TodoItemsComponent,
+    TodoItemComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
