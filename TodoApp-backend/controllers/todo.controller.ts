3835b53f23460fcb48c85dbff4f5f09206e6bb29
deekeej <daniel.krejza@gmail.com>
2022-08-04 22:21:17 +0200

    backend: todos endpoint finished

--- TodoApp-backend/controllers/todo.controller.ts
+++ TodoApp-backend/controllers/todo.controller.ts
@@ -1,20 +1,21 @@
 import { Request, Response } from "express";
 import { con } from "../database/database";
+import { TodoModel } from "../types/todoModel";
 
 export const getTodos = async (req: Request, res: Response) => {
   try {
-    con.query("SELECT * FROM users", (err, rows) => {
+    let results: TodoModel[] = [];
+    con.query("SELECT id,title,completed FROM todos", (err, rows) => {
       if (err) throw err;
-
-      console.log("Data received from Db:");
+      let todos = JSON.parse(JSON.stringify(rows));
       console.log(rows);
+      res.status(200).send(todos);
     });
     // con.end((err) => {
     //   // The connection is terminated gracefully
     //   // Ensures all remaining queries are executed
     //   // Then sends a quit packet to the MySQL server.
     // });
-    res.status(200);
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
--- TodoApp-backend/controllers/user.controller.ts
+++ TodoApp-backend/controllers/user.controller.ts
@@ -7,14 +7,14 @@ export const getUsers = async (req: Request, res: Response) => {
       if (err) throw err;
 
       console.log("Data received from Db:");
-      console.log(rows);
+      console.log(rows.username);
     });
     // con.end((err) => {
     //   // The connection is terminated gracefully
     //   // Ensures all remaining queries are executed
     //   // Then sends a quit packet to the MySQL server.
     // });
-    res.status(200);
+    res.status(200).send();
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
