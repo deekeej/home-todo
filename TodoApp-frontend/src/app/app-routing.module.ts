524a5fa1e1c7ef41885b46997047853731b8e39f
deekeej <daniel.krejza@gmail.com>
2022-08-07 00:51:28 +0200

    solved problem with loading image + register html

--- TodoApp-frontend/src/app/app-routing.module.ts
+++ TodoApp-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/authentification/register/register.component';
 import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [{ path: '', component: TodosComponent }];
+const routes: Routes = [{ path: '', component: RegisterComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TodoApp-frontend/src/app/components/todos/todos-parts/todo-list/todo-list.component.ts
+++ TodoApp-frontend/src/app/components/todos/todos-parts/todo-list/todo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoModel } from '../../todos-store/state';
+import { TodoModel } from 'src/app/todos-store/state';
 import { Store } from '@ngrx/store';
-import { todosSelector } from '../../todos-store/selectors';
+import { todosSelector } from 'src/app/todos-store/selectors';
 
 @Component({
   selector: 'app-todo-list',
