45a70b1329fcd9068097beecebc2b0ea14bf1f12
deekeej <daniel.krejza@gmail.com>
2022-08-06 23:39:15 +0200

    refactoring of todos and add authentification

---
+++ TodoApp-frontend/src/app/components/todos/todos-parts/todo-input/todo-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TodoModel } from '../../../../todos-store/state';
+import { todosSelector } from '../../../../todos-store/selectors';
+import { actions } from '../../../../todos-store/actions';
+@Component({
+  selector: 'app-todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.scss'],
+})
+export class TodoInputComponent implements OnInit {
+  todoInput?: string;
+  todos?: TodoModel[];
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.select(todosSelector).subscribe((state) => (this.todos = state));
+  }
+
+  addTodo() {
+    if (this.todoInput != '' || this.todoInput != null) {
+    }
+    this.store.dispatch(
+      actions.addTodoAction({
+        id: this.todos!.length,
+        title: this.todoInput!,
+        completed: false,
+      })
+    );
+    this.todoInput = '';
+  }
+}
